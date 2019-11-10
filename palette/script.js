9e1a7ef889a855f3ae5a7f392edc14e64fd5052f
Mikita Maiseichek <Doktornikita@hotmail.com>
2019-11-11 00:04:39 +0300

    feat: add keyboard controls KeyB, KeyC, KeyP

--- palette/script.js
+++ palette/script.js
@@ -1,13 +1,16 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-let click = document.querySelector('.color');
-let currentColor = '#00ff00';
+const colorGet = document.querySelector('.color');
+const selectTool = document.querySelector('.tools');
 const tools = {
     fillBucket: false,
     chooseColor: false,
     pencil: true
 };
 
+canvas.width = 512;
+canvas.height = 512;
+const matrix = 128;
 
 function takeColor() {
     return document.querySelector('#current').value;
@@ -20,8 +23,23 @@ function setPrevColor() {
     prevColor.setAttribute('data', `${temp}`);
 }
 
-click.onclick = function (event) {
-    console.dir(event.target);
+const getColorAtPixel = (imageData, x, y) => {
+    const {width, data} = imageData;
+
+    return {
+        r: data[4 * (width * y + x)],
+        g: data[4 * (width * y + x) + 1],
+        b: data[4 * (width * y + x) + 2],
+        a: data[4 * (width * y + x) + 3]
+    };
+};
+
+function rgbToHex(r, g, b) {
+    return ((r << 16) | (g << 8) | b).toString(16);
+}
+
+
+colorGet.onclick = function (event) {
     let current = document.querySelector('#current');
     if (event.target.innerText === 'blue') {
         setPrevColor();
@@ -36,9 +54,18 @@ click.onclick = function (event) {
     }
 };
 
-canvas.width = 512;
-canvas.height = 512;
-const matrix = 128;
+selectTool.onclick = function (event) {
+    tools.fillBucket = false;
+    tools.chooseColor = false;
+    tools.pencil = false;
+    if (event.target.innerText === 'Fill bucket') {
+        tools.fillBucket = true;
+    } else if (event.target.innerText === 'Choose color') {
+        tools.chooseColor = true;
+    } else if (event.target.innerText === 'Pencil') {
+        tools.pencil = true;
+    }
+};
 
 canvas.onmousedown = function (event) {
     if (tools.pencil === true) {
@@ -54,11 +81,43 @@ canvas.onmousedown = function (event) {
             ctx.fill();
             ctx.fillRect(x * matrix, y * matrix, matrix, matrix);
         };
+        canvas.onmouseleave = function () {
+            canvas.onmousemove = null;
+        };
         canvas.onmouseup = function () {
             canvas.onmousemove = null;
         };
+    } else if (tools.chooseColor === true) {
+        let x = event.offsetX;
+        let y = event.offsetY;
+        let current = document.querySelector('#current');
+        let colorPick = ctx.getImageData(0, 0, 512, 512);
+        setPrevColor();
+        let objColor = getColorAtPixel(colorPick, x, y);
+        current.value = '#' + ('000000' + rgbToHex(objColor.r, objColor.g, objColor.b)).slice(-6);
+
+    } else if (tools.fillBucket === true) {
+        ctx.fillStyle = `${takeColor()}`;
+        ctx.fill();
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
     }
 };
+
+document.addEventListener('keydown', (event) => {
+    switch (event.code) {
+        case 'KeyB':
+            selectTool.children[0].click();
+            break;
+
+        case 'KeyP':
+            selectTool.children[2].click();
+            break;
+
+        case 'KeyC':
+            selectTool.children[1].click();
+            break;
+    }
+});
 
 
 
