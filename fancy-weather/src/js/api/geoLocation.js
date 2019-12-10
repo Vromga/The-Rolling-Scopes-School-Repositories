608cc49dbc907931baacaffb134cdfd3a7c41ecf
Mikita Maiseichek <Doktornikita@hotmail.com>
2019-12-10 17:24:43 +0300

    feat: add geoLocation

---
+++ fancy-weather/src/js/api/geoLocation.js
@@ -0,0 +1,20 @@
+import {keyGeoLocation} from "../configuration";
+
+function geoNative() {
+  var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+
+  function success(pos) {
+    var crd = pos.coords;
+    option.center = [crd.longitude, crd.latitude];
+  };
+
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  };
+
+  navigator.geolocation.getCurrentPosition(success, error, options);
+}
--- fancy-weather/src/js/configuration.js
+++ fancy-weather/src/js/configuration.js
@@ -1,7 +1,10 @@
- const lang = [
+const lang = [
   'EN', 'RU', 'BY'
 ];
 
+const keyWeather = '36991b1be3297e1561963c6ed47c155e';
+const keyGeoLocation = '1db8194a2703a0';
+const imageKey = 'f258bef5c6ebd9401bba43395eb2fd21';
 
 
-export {lang};
+export {lang, keyWeather, keyGeoLocation, imageKey};
