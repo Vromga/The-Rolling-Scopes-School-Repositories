3f6260c4d4d81e4ff8c875fc4ba3ebd33576206d
Mikita Maiseichek <Doktornikita@hotmail.com>
2019-12-22 22:10:19 +0300

    feat: add logic select image

--- fancy-weather/src/js/DOM/bgImage.js
+++ fancy-weather/src/js/DOM/bgImage.js
@@ -3,14 +3,22 @@ import loader from "../utilities/loader";
 
 function createBackground(data) {
   const photosArr = data.photos.photo;
-  const photoURLArr = photosArr.filter(value => {
-    if (value.height_o) return value;
-  });
-
+  const docWidth = document.documentElement.offsetWidth;
   const body = document.querySelector('body');
-  const photo = photoURLArr[getRandomPhoto(photoURLArr.length)];
-  body.style.background = `url("${photo.url_o}") center center / cover no-repeat fixed`;
-  loader();
+
+  if (docWidth < 1024) {
+    const photo = photosArr[getRandomPhoto(photosArr.length)];
+    const url = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
+    body.style.background = `url("${url}") center center / cover no-repeat fixed`;
+  } else {
+    const photoURLArr = photosArr.filter(value => {
+      if (value.height_o) return value;
+    });
+    const photo = photoURLArr[getRandomPhoto(photoURLArr.length)];
+    body.style.background = `url("${photo.url_o}") center center / cover no-repeat fixed`;
+  }
+
+  loader()
 }
 
 export default createBackground;
