3ed373508c8238230c5f206185e827dd3f331c59
Mikita Maiseichek <Doktornikita@hotmail.com>
2019-12-29 20:42:24 +0300

    feat: add change size for all canvas

--- simple-piskel-clone/src/js/model/changeSizeCanvas.js
+++ simple-piskel-clone/src/js/model/changeSizeCanvas.js
@@ -5,18 +5,28 @@ import setActiveElement from "../view/setActiveElement";
 
 function changeSizeCanvas(e) {
   const mainCanvas = document.querySelector('.main--draw_container-canvas');
+  const layerCanvas = document.querySelector('.layer--canvas');
+  const frameCanvas = document.querySelectorAll('.frame--preview-canvas');
+
   if (e.target.getAttribute('data') === '32') {
       removeAttributeData(mainCanvas);
-      setSizeCanvasAttribute(mainCanvas, e.target.getAttribute('data'));
+      setSizeCanvasAttribute(mainCanvas, e.target.getAttribute('data'), false);
+      setSizeCanvasAttribute(layerCanvas, e.target.getAttribute('data'), false);
+      setSizeCanvasAttribute(frameCanvas, e.target.getAttribute('data'), true);
       setActiveElement(e);
   }else if(e.target.getAttribute('data') === '64'){
     removeAttributeData(mainCanvas);
-    setSizeCanvasAttribute(mainCanvas, e.target.getAttribute('data'));
+    setSizeCanvasAttribute(mainCanvas, e.target.getAttribute('data'), false);
+    setSizeCanvasAttribute(layerCanvas, e.target.getAttribute('data'), false);
+    setSizeCanvasAttribute(frameCanvas, e.target.getAttribute('data'), true);
     setActiveElement(e);
   }else if (e.target.getAttribute('data') === '128'){
     removeAttributeData(mainCanvas);
-    setSizeCanvasAttribute(mainCanvas, e.target.getAttribute('data'));
+    setSizeCanvasAttribute(mainCanvas, e.target.getAttribute('data'), false);
+    setSizeCanvasAttribute(layerCanvas, e.target.getAttribute('data'), false);
+    setSizeCanvasAttribute(frameCanvas, e.target.getAttribute('data'), true);
     setActiveElement(e);
+
   }
 }
 
--- simple-piskel-clone/src/js/utilits/setSizeCanvasAttribute.js
+++ simple-piskel-clone/src/js/utilits/setSizeCanvasAttribute.js
@@ -1,8 +1,15 @@
 import {canvasSize} from "../configuration";
 
-function setSizeCanvasAttribute(elem, attr) {
-  elem.setAttribute('width', `${canvasSize[attr]}`);
-  elem.setAttribute('height', `${canvasSize[attr]}`);
+function setSizeCanvasAttribute(elem, attr, collection) {
+  if (collection) {
+    elem.forEach((v) => {
+      v.setAttribute('width', `${canvasSize[attr]}`);
+      v.setAttribute('height', `${canvasSize[attr]}`);
+    })
+  } else {
+    elem.setAttribute('width', `${canvasSize[attr]}`);
+    elem.setAttribute('height', `${canvasSize[attr]}`);
+  }
 }
 
 export default setSizeCanvasAttribute;
