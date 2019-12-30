ca5bbf8d4a2ca5b9b4e7a2639fc664f582a42d82
Mikita Maiseichek <Doktornikita@hotmail.com>
2019-12-30 10:58:29 +0300

    fix: fix eraser

--- simple-piskel-clone/src/js/model/pencilAndEraserLogic/setPixel.js
+++ simple-piskel-clone/src/js/model/pencilAndEraserLogic/setPixel.js
@@ -12,17 +12,16 @@ function setPixel(x, y, e) {
   const virtualPixel = localStorage.getItem('virtualPixel');
   ctx.beginPath();
   if (tools.eraser === true) {
-    ctx.fillStyle = `rgba(0, 0, 0, 0)`;
+    ctx.clearRect(x, y, +virtualPixel, +virtualPixel);
   } else if (tools.pencil === true) {
     if (e.which === LEFT_MOUSE_BUTTON) {
       ctx.fillStyle = getColorPicker().primary;
     } else if (e.which === RIGHT_MOUSE_BUTTON) {
       ctx.fillStyle = getColorPicker().secondary;
     }
+    ctx.fill();
+    ctx.fillRect(x, y, +virtualPixel, +virtualPixel);
   }
-
-  ctx.fill();
-  ctx.fillRect(x, y, +virtualPixel, +virtualPixel);
   cordPixel.x0 = cordPixel.x1;
   cordPixel.y0 = cordPixel.y1;
 }
