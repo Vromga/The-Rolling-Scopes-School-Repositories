0e6cfbdfb9904923301001925331fd65bc65dc45
Mikita Maiseichek <Doktornikita@hotmail.com>
2020-01-09 10:45:06 +0300

    fix: fix vk auth bug and bug hot key

--- simple-piskel-clone/src/app/js/controller/chooseToolsKeyboard.js
+++ simple-piskel-clone/src/app/js/controller/chooseToolsKeyboard.js
@@ -1,15 +1,15 @@
 import resetToolsState from "../utilits/resetToolsState";
-import {tools} from "../configuration";
+import {key, tools} from "../configuration";
 import saveStateApp from "../model/saveApp/saveStateApp";
 import setActiveElementKeyboard from "../model/keybord/setActiveElementKeyboard";
 
 function chooseToolsKeyboard(e) {
 	resetToolsState();
-	if (e.code === 'KeyP') tools.pencil = true;
-	if (e.code === 'KeyA') tools.fillAllPixels = true;
-	if (e.code === 'KeyE') tools.eraser = true;
-	if (e.code === 'KeyL') tools.stroke = true;
-	if (e.code === 'KeyO') tools.colorPicker = true;
+	if (e.code === `${key.pencil}`) tools.pencil = true;
+	if (e.code === `${key.fillAllPixels}`) tools.fillAllPixels = true;
+	if (e.code === `${key.eraser}`) tools.eraser = true;
+	if (e.code === `${key.stroke}`) tools.stroke = true;
+	if (e.code === `${key.colorPicker}`) tools.colorPicker = true;
 	setActiveElementKeyboard();
 	const jsonTools = JSON.stringify(tools);
 	saveStateApp('tools', jsonTools);
